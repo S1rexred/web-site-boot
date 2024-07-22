b37c3187918b41f6c13a7bc07418cf6eff4b5a83
S1rexred <165589614+S1rexred@users.noreply.github.com>
2024-07-22 15:34:18 +0400

    4

--- src/Components/Navibar.jsx
+++ src/Components/Navibar.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Button, Container, Nav, Navbar, Container as BootstrapContainer } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button, Container, Nav, Navbar, Modal, Form } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -13,6 +13,14 @@ const Styles = styled.div `
 `
 
 export default function NaviBar() {
+
+
+    const[show, setShow] = useState(false)
+
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
+
+
   return (
   <>
   <Styles>
@@ -27,13 +35,34 @@ export default function NaviBar() {
           <Nav.Link><Link to='/users'>Пользователи</Link></Nav.Link>
           </Nav>
           <Nav>
-            <Button variant='primary' className="mr-2">Войти</Button>
-            <Button variant='primary'>Регистрация</Button>
+            <Button variant='primary' className="mr-2" onClick={handleShow}>Войти</Button>
+            <Button variant='primary' onClick={handleShow}>Регистрация</Button>
           </Nav>
       </Navbar.Collapse>
       </Container>
     </Navbar>
     </Styles>
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+      <Modal.Title>Вход</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group controlId='fromBasicEmail'>
+              <Form.Label>Почта</Form.Label>
+              <Form.control type='Ваша почта' placeholder='Введите почту'/>
+              <Form.text className='text-muted'>1111</Form.text>
+          </Form.Group>
+          <Form.Group controlId='fromBasicPassword'>
+              <Form.Label>Пароль</Form.Label>
+              <Form.control type='Ваш пароль' placeholder='Введите пароль'/>
+          </Form.Group>
+          <Form.Group controlId='fromBasicCheckbox'>
+              <Form.check type='checkbox' label='r'/>
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+    </Modal>
   </>
 )
 }
