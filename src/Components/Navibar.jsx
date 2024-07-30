e0e7d4014fa411dbee6cf5e356852c58c87891b6
S1rexred <165589614+S1rexred@users.noreply.github.com>
2024-07-31 01:34:02 +0400

    16

--- src/Components/Navibar.jsx
+++ src/Components/Navibar.jsx
@@ -3,6 +3,7 @@ import { Button, Container, Nav, Navbar, Modal, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+
 const Styles = styled.div`
   a,
   .navbar-brand,
@@ -15,46 +16,70 @@ const Styles = styled.div`
   }
 `;
 
-export default function NaviBar() {
+
+const NaviBar = () => {
   const [show, setShow] = useState(false);
   const [showSign, setShowSign] = useState(false);
+  const [userEmail, setUserEmail] = useState(null);
+  const [loginEmail, setLoginEmail] = useState(''); // Состояние для хранения почты при входе
+  const [loginPassword, setLoginPassword] = useState(''); // Состояние для хранения пароля при входе
+  const [registerEmail, setRegisterEmail] = useState(''); // Состояние для хранения почты при регистрации
+  const [registerPassword, setRegisterPassword] = useState(''); // Состояние для хранения пароля при регистрации
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   const handleCloseSign = () => setShowSign(false);
-  const handleShowSign = () => setShowSign(true);
+
+  const handleLogin = () => {
+    
+    setUserEmail(loginEmail); // Устанавливаем почту пользователя из формы
+    handleClose(); // Закрываем модальное окно
+  };
+
+  const handleRegister = () => {
+    
+    setUserEmail(registerEmail); // Устанавливаем почту пользователя из формы
+    handleCloseSign(); // Закрываем модальное окно
+  };
 
   return (
     <>
-      <Styles>
-        <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
-          <Container>
-            <Navbar.Brand>Block WW</Navbar.Brand>
-            <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-            <Navbar.Collapse id='responsive-navbar-nav'>
-              <Nav className='mr-auto'>
-                <Nav.Link>
-                  <Link to='/'>Главная</Link>
-                </Nav.Link>
-                <Nav.Link>
-                  <Link to='/about'>О нас</Link>
-                </Nav.Link>
-                <Nav.Link>
-                  <Link to='/menu'>Меню</Link>
-                </Nav.Link>
-              </Nav>
-              <Nav>
-                <Button variant='primary' className='mr-2' onClick={handleShow}>
+    <Styles>
+      <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+        <Container>
+          <Navbar.Brand>Block WW</Navbar.Brand>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='mr-auto'>
+              <Nav.Link>
+                <Link to='/'>Главная</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to='/about'>О нас</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to='/menu'>Меню</Link>
+              </Nav.Link>
+            </Nav>
+            {userEmail ? (
+              <div>
+                <p style={{ color: 'white' }}>{userEmail}</p>
+                <Button variant="secondary" onClick={() => setUserEmail(null)}>
+                  Выйти
+                </Button>
+              </div>
+            ) : (
+              <div>
+                <Button style={{ marginRight: '5px' }} variant="primary" onClick={() => setShow(true)}>
                   Войти
                 </Button>
-                <Button variant='primary' onClick={handleShowSign}>
+                <Button variant="secondary" onClick={() => setShowSign(true)}>
                   Регистрация
                 </Button>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+              </div>
+            )}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       </Styles>
 
       <Modal show={show} onHide={handleClose}>
@@ -63,19 +88,24 @@ export default function NaviBar() {
         </Modal.Header>
         <Modal.Body>
           <Form>
-            <Form.Group controlId='fromBasicEmail'>
+            <Form.Group controlId='formBasicEmail'>
               <Form.Label>Почта</Form.Label>
-              <Form.Control type='email' placeholder='Введите почту' />
-              <Form.Text className='text-muted'></Form.Text>
+              <Form.Control 
+                type='email' 
+                placeholder='Введите почту' 
+                value={loginEmail}
+                onChange={(e) => setLoginEmail(e.target.value)} // Обновляем состояние при вводе
+              />
             </Form.Group>
-            <Form.Group controlId='fromBasicPassword'>
+            <Form.Group controlId='formBasicPassword'>
               <Form.Label>Пароль</Form.Label>
-              <Form.Control type='password' placeholder='Введите пароль' />
+              <Form.Control type='password' 
+                placeholder='Введите пароль' 
+                value={loginPassword}
+                onChange={(e) => setLoginPassword(e.target.value)} // Обновляем состояние при вводе
+              />
             </Form.Group>
-            <Form.Group controlId='fromBasicCheckbox'>
-              <Form.Check type='checkbox' label='Запомнить устройство' />
-            </Form.Group>
-            <Button variant='primary' style={{marginTop: '8px'}} onClick={handleClose}>
+            <Button variant='primary' style={{ marginTop: '8px' }} onClick={handleLogin}>
               Войти
             </Button>
           </Form>
@@ -88,24 +118,29 @@ export default function NaviBar() {
         </Modal.Header>
         <Modal.Body>
           <Form>
-            <Form.Group controlId='fromBasicEmail'>
+            <Form.Group controlId='formBasicName'>
               <Form.Label>Имя</Form.Label>
-              <Form.Control type='name' placeholder='Введите ваше имя' />
-              <Form.Text className='text-muted'></Form.Text>
+              <Form.Control type='text' placeholder='Введите ваше имя' />
             </Form.Group>
-            <Form.Group controlId='fromBasicEmail'>
+            <Form.Group controlId='formBasicEmail'>
               <Form.Label>Почта</Form.Label>
-              <Form.Control type='email' placeholder='Введите почту' />
-              <Form.Text className='text-muted'></Form.Text>
+              <Form.Control 
+                type='email' 
+                placeholder='Введите почту' 
+                value={registerEmail}
+                onChange={(e) => setRegisterEmail(e.target.value)} // Обновляем состояние при вводе
+              />
             </Form.Group>
-            <Form.Group controlId='fromBasicPassword'>
+            <Form.Group controlId='formBasicPassword'>
               <Form.Label>Пароль</Form.Label>
-              <Form.Control type='password' placeholder='Введите пароль' />
+              <Form.Control 
+                type='password' 
+                placeholder='Введите пароль' 
+                value={registerPassword}
+                onChange={(e) => setRegisterPassword(e.target.value)} // Обновляем состояние при вводе
+              />
             </Form.Group>
-            <Form.Group controlId='fromBasicCheckbox'>
-              <Form.Check type='checkbox' label='Запомнить устройство' />
-            </Form.Group>
-            <Button variant='primary' style={{marginTop: '8px'}} onClick={handleCloseSign}>
+            <Button variant='primary' style={{ marginTop: '8px' }} onClick={handleRegister}>
               Зарегистрироваться
             </Button>
           </Form>
@@ -113,4 +148,6 @@ export default function NaviBar() {
       </Modal>
     </>
   );
-}
+};
+
+export default NaviBar;
